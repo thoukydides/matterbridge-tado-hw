b535974a9cc918636cc726555658ec2940bae97b
Alexander Thoukydides <thoukydides@users.noreply.github.com>
2026-02-12 08:47:22 +0000

    Matterbridge 3.5.4 compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 // Matterbridge plugin for Tado hot water control
 // Copyright © 2025 Alexander Thoukydides
 
-import { Matterbridge, PlatformConfig } from 'matterbridge';
+import { PlatformMatterbridge, PlatformConfig } from 'matterbridge';
 import { AnsiLogger } from 'matterbridge/logger';
 import { TadoHWPlatform } from './platform.js';
 
 // Register the platform with Matterbridge
 export default function initializePlugin(
-    matterbridge:   Matterbridge,
+    matterbridge:   PlatformMatterbridge,
     log:            AnsiLogger,
     config:         PlatformConfig
 ): TadoHWPlatform {
